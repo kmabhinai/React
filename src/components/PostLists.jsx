41d17f64bc5ec6c5e9702c534e4118256bec39d4
kmabhinai <kmabhinai@gmail.com>
2023-03-05 07:31:08 +0530

    Added Modal

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import Post from "./post";
+import Modal from "./Modal";
 import NewPost from "./NewPost";
 import classes from "./PostsLists.module.css";
 
 function PostLists() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
     return (
         <>
-            <NewPost />
+            {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost
+                        onBodyChange={bodyChangeHandler}
+                        onAuthorChange={authorChangeHandler}
+
+                    />
+                </Modal>
+            )}
             <ul className={classes.posts}>
-                <Post author="asd" body="asd" />
-                <Post body="asdsad" />
+                <Post author={enteredAuthor} body={enteredBody} />
             </ul>;
         </>
     );
